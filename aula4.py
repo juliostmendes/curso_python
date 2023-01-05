198ede75e7074b8d7bacd07495b5f3e1f69c3150
juliostmendes <juliostmendes@outlook.com>
2023-01-04 21:13:53 -0300

    Adicionando aulas 6, 7 e 8

--- aula4.py
+++ aula4.py
@@ -1,4 +1,4 @@
-# Tipos int e float
+# Assuntos: Tipos int e float
 # int -> Número inteiro
 # O tipo int representa qualquer número
 # positivo ou negativo. int sem sinal é considerado
---
+++ aula7.py
@@ -0,0 +1,15 @@
+# Assuntos: Variáveis
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+
+nome = 'Julio'
+idade = 19
+# Toda vez que se joga uma expressão em uma variável ela é bool
+maior_de_idade = idade >= 18 
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
