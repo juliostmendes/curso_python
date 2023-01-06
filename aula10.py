ce78defe89c54b5b353a4d46d2f1236bab43e607
juliostmendes <juliostmendes@outlook.com>
2023-01-06 00:00:32 -0300

    Aulas 9, 10 e 11

---
+++ aula10.py
@@ -0,0 +1,9 @@
+# Assunto: concatenação
+# Só posso concatenar str
+concatenacao = 'Julio' + ' ' + 'Mendes'  # Tomar cuidado com a tipagem
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_eu = 3 * 'eu'
+print(a_dez_vezes)
+print(tres_vezes_eu)
