a776fed15fda984b5b1ed5c0b8445c5597a8b8fa
juliostmendes <juliostmendes@outlook.com>
2023-01-08 23:25:09 -0300

    Aulas 20 e 21

---
+++ aula20.py
@@ -0,0 +1,11 @@
+
+# Exercício
+numero_1 = input('Digite o primeiro valor: ')
+numero_2 = input('Digite o segundo valor: ')
+
+if numero_1 > numero_2:
+    print(f'{numero_1= } é maior que o {numero_2= }')
+elif numero_2 > numero_1:
+    print(f'{numero_2= } é maior que o {numero_1= }')
+else:
+    print('Os valores são iguais')
