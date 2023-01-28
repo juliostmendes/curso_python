408d700502f38ff1b0ca7c7b4e796c602860e17e
Julio Mendes <juliostmendes@outlook.com>
2023-01-27 22:06:05 -0300

    adicionando aulas 33 a 35

---
+++ aula33.py
@@ -0,0 +1,15 @@
+"""
+Assunto: tipos build-in
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+# Os tipos são objetos que podem ter diferentes métodos
+
+nome = 'Julio Mendes'
+nome_maiusculo = nome.upper()  # Um exemplo de método de uma str
+print(f'Nome maiusculo = {nome_maiusculo}')
+#string = '1000'
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# print(string)
+# print(outra_variavel)
+#print(string.zfill(10))
