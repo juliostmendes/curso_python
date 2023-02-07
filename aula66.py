3bb85d6833873479233114d0a49af05d138aa5c5
juliostmendes <juliostmendes@outlook.com>
2023-02-07 17:07:17 -0300

    Argumentos nomeados

--- aula66.py
+++ aula66.py
@@ -1,5 +1,5 @@
 """
-Argumentos nomeados e não nomeados em funções Python
+Assunto: Argumentos nomeados e não nomeados em funções Python
 Argumento nomeado tem nome com sinal de igual
 Argumento não nomeado recebe apenas o argumento (valor)
 """
@@ -11,5 +11,5 @@ def soma(x, y, z):
 
 
 soma(1, 2, 3)
-soma(1, y=2, z=5)
+soma(1, y=2, z=5)  # Por estar nomeado posso passar em qualquer ordem
 print(1, 2, 3, sep='-')
