e6777d325a750df66fb35689f1907b805dc13ab7
juliostmendes <juliostmendes@outlook.com>
2023-02-15 19:33:57 -0300

    Função Lambda

---
+++ aula82.py
@@ -0,0 +1,21 @@
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+# def soma(x, y):
+#     return x + y
+
+# def cria_multiplicador(multiplicador):
+#     def multiplica(numero):
+#         return numero * multiplicador
+#     return multiplica
+
+# duplica = cria_multiplicador(2)
+
+# A lambda serve como uma função que pode ser definida em uma linha
+duplica = executa(lambda m: lambda n: n * m, 2)
+print(duplica(2))
+
+print(executa(lambda x, y: x + y, 2, 3), )
+
+print(executa(lambda *args: sum(args), 1, 2, 3, 4, 5, 6, 7))
